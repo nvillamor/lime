60a0ff6969b7f6f7ef359aeb5c9bcb6194efa2cb
Nicolás Villamor <villamorn@hotmail.comgit config --global user.email villamorn@hotmail.comgit config --global user.email villamorn@hotmail.com>
2023-01-18 20:27:31 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,108 @@
+
+var pass = 'mi-texto';
+var bloqueo = 'false';
+var tiempo = 0;
+
+var connection = new WebSocket('ws://' + location.hostname + '/ws', ['arduino']);
+
+connection.onopen = function () {
+	console.log('Connected: ');
+};
+
+connection.onerror = function (error) {
+  console.log('WebSocket Error ', error);
+};
+
+connection.onmessage = function (e) {
+  console.log('Server: ', e.data);
+  processData(e.data);
+};
+
+connection.onclose = function () {
+  console.log('WebSocket connection closed');
+};
+
+function sendData(elemento)
+{
+
+  console.log(elemento.id);
+  console.log(elemento.value);  
+
+  let data = {
+	  command : "Set",
+	  id: elemento.id,
+	  status: elemento.value
+  }
+  let json = JSON.stringify(data);
+
+  connection.send(json);
+}
+
+
+function getData()
+{
+  let data = {
+	  command : "Get"
+  }
+  let json = JSON.stringify(data);
+
+  connection.send(json);
+}
+
+function processData(data)
+{
+  let json = JSON.parse(data); 
+  console.log(json);
+  let receivedMsg = 'Received: GPIO ' + json.id + ' ' + (json.status == 1 ? "ON" : "OFF");
+  document.getElementById('receivedText').textContent = receivedMsg;
+}
+
+function ChangePass(elemento) {
+  var checkBox = document.getElementById("bloqueo");
+    if (checkBox.checked) {   
+    pass = document.getElementById('password').value;
+    document.getElementById('CambiarPass').value = pass;
+    
+    document.getElementById('password').value = "";
+    document.getElementById('password').placeholder = "Contraseña modificada";
+    sendData(elemento);
+  }
+}
+
+function myFunction(elemento) {
+  
+  var checkBox = document.getElementById("bloqueo");
+  var text = document.getElementById('password').value;
+    if (text === pass) {
+      if (checkBox.checked) {
+        document.getElementById('estado').innerHTML = "Monopatín DESBLOQUEADO";
+        document.getElementById('estado').style.color = "red";
+        document.getElementById('estado2').hidden = false;
+        document.getElementById('password').value = "";
+        document.getElementById('password').placeholder = "Ingrese nueva contraseña";
+      } else {
+        BLOQUEO();
+      } 
+
+    } else {
+      if(checkBox.checked){
+        document.getElementById('password').value = "";
+        document.getElementById('password').placeholder = "Contraseña INCORRECTA";
+        BLOQUEO();
+      } else { 
+        BLOQUEO();
+        document.getElementById('password').placeholder = "Ingrese contraseña";
+      }
+    }
+
+    elemento.value = document.getElementById('bloqueo').checked;
+    sendData(elemento);
+  };
+
+  function BLOQUEO() {
+    document.getElementById('estado').innerHTML = "Monopatín BLOQUEADO";
+    document.getElementById('estado').style.color = "green";
+    document.getElementById('bloqueo').checked = false;
+    document.getElementById('estado2').hidden = true;
+  }
+
