f8bb3823f91b21af94236e346623d5cb7ac4fa70
Nicolás Villamor <36418450+nvillamor@users.noreply.github.com>
2023-01-25 21:36:31 -0300

    NUEVO!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 
 var pass = 'mi-texto';
-var bloqueo = 'false';
+var bloqueo = false;
 var tiempo = 0;
-
+var nopass = false;
 var connection = new WebSocket('ws://' + location.hostname + '/ws', ['arduino']);
+
+window.addEventListener('DOMContentLoaded', 
+  function() { 
+        NoPass();
+  }, false);
+
+function NoPass() { 
+    var checkBoxNoPass = document.getElementById("nopass");
+    if (checkBox.checked) {
+      alert('check');
+    } else {
+      alert('uncheck');
+    }
+}
+
 
 connection.onopen = function () {
 	console.log('Connected: ');
+  getData();
 };
 
 connection.onerror = function (error) {
@@ -53,19 +69,46 @@ function processData(data)
 {
   let json = JSON.parse(data); 
   console.log(json);
-  let receivedMsg = 'Received: GPIO ' + json.id + ' ' + (json.status == 1 ? "ON" : "OFF");
-  document.getElementById('receivedText').textContent = receivedMsg;
+
+  pass = json.pass;
+  bloqueo = json.bloqueo; 
+  tiempo = json.tiempo;
+
+  if (bloqueo === true)  {
+    document.getElementById('bloqueo').checked = true;
+  } else if (bloqueo === false) {
+    document.getElementById('bloqueo').checked = false;
+  }
+
+  switch (tiempo) {
+   case 0:
+    document.getElementById('CambiarTiempo1').checked = true;
+    break;
+   case 60:
+    document.getElementById('CambiarTiempo2').checked = true;
+    break;
+   case 300: 
+   document.getElementById('CambiarTiempo3').checked = true;
+       break;
+   case -1:
+    document.getElementById('CambiarTiempo4').checked = true;
+    break;
+
+   default: 
+       alert('Default case');
+  }
+  
 }
 
 function ChangePass(elemento) {
   var checkBox = document.getElementById("bloqueo");
     if (checkBox.checked) {   
-    pass = document.getElementById('password').value;
+    pass = document.getElementById('ChangePass').value;
     document.getElementById('CambiarPass').value = pass;
     
-    document.getElementById('password').value = "";
-    document.getElementById('password').placeholder = "Contraseña modificada";
-    document.getElementById('CambiarPass').hidden = true;
+    document.getElementById('ChangePass').value = "";
+    document.getElementById('ChangePass').placeholder = "Contraseña modificada";
+    //document.getElementById('CambiarPass').hidden = true;
     sendData(elemento);
   }
 }
@@ -76,30 +119,19 @@ function myFunction(elemento) {
   var text = document.getElementById('password').value;
     if (text === pass) {
       if (checkBox.checked) {
-        document.getElementById('estado').innerHTML = "Monopatín DESBLOQUEADO";
-        document.getElementById('estado').style.color = "red";
-        document.getElementById('CambiarPass').hidden = false;
-        document.getElementById('password').value = "";
-        document.getElementById('password').placeholder = "Ingrese nueva contraseña";
-        const collection = document.getElementsByName("TiempoBloqueo");
-        for (let i = 0; i < collection.length; i++) {
-          collection[i].disabled = false;
-        }
+        DESBLOQUEO();
       } else {
         BLOQUEO();
       } 
-
     } else {
       if(checkBox.checked){
         document.getElementById('password').value = "";
-        document.getElementById('password').placeholder = "Contraseña INCORRECTA";
         BLOQUEO();
+        document.getElementById('password').placeholder = "Contraseña INCORRECTA";
       } else { 
         BLOQUEO();
-        document.getElementById('password').placeholder = "Ingrese contraseña";
       }
     }
-
     elemento.value = document.getElementById('bloqueo').checked;
     sendData(elemento);
   };
@@ -109,26 +141,35 @@ function myFunction(elemento) {
     document.getElementById('password').placeholder = "Ingrese contraseña";
     document.getElementById('estado').style.color = "green";
     document.getElementById('bloqueo').checked = false;
-    document.getElementById('CambiarPass').hidden = true;
-    document.getElementsByName('TiempoBloqueo').disabled = true;
-    
-    const collection = document.getElementsByName("TiempoBloqueo");
+    //document.getElementById('CambiarPass').hidden = true;
+    document.getElementById('password').hidden = false;
+    document.getElementById('config-button').hidden = true;
+    document.querySelector('.contendorForm').style = "display: none"; 
+    const collection = document.getElementsByName('TiempoBloqueo');
     for (let i = 0; i < collection.length; i++) {
       collection[i].disabled = true;
     }
-    
-
+    console.log(pass);
   }
 
-/* funciones dvdcom */
+  function DESBLOQUEO() {
+    document.getElementById('config-button').hidden = false;
+    document.getElementById('estado').innerHTML = "Monopatín DESBLOQUEADO";
+    document.getElementById('estado').style.color = "red";
+    //document.getElementById('CambiarPass').hidden = false;
+    document.getElementById('password').hidden = true;
+    document.getElementById('password').value = "";
+    document.getElementById('ChangePass').placeholder = "Ingrese nueva contraseña";
+    const collection = document.getElementsByName("TiempoBloqueo");
+    for (let i = 0; i < collection.length; i++) {
+      collection[i].disabled = false;
+    }
+  }
+
+  /* funciones dvdcom */
 
 function MostrarConfiguracion(){
   const panel = document.querySelector('.contendorForm');
   const display = panel.getAttribute('style');
   display === "display: none" ? panel.setAttribute("style","display: block") : panel.setAttribute("style","display: none");
 }
-
-function cambiarPass(){
-  console.log('Codigo que cambia la contraseña');
-}
-
